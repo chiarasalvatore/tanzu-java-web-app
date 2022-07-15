95e0073b09720dc589154b9171696312e28cfc9d
chiarasalvatore <108433497+chiarasalvatore@users.noreply.github.com>
2022-07-15 18:21:28 +0200

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 @WebMvcTest(HelloController.class)
 class HelloControllerTest {
